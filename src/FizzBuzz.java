32d1ef040d61f2bb9efc37b4cecdc55024ccd472
paianish62 <paianish62@gmail.com>
2024-09-09 18:17:06 -0400

    Used FizzBuzz Class in Main

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,7 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
         int i = 1;
         while (i < 100) {
 
@@ -12,7 +12,7 @@ class FizzBuzz {
         }
     }
 
-    private static int dofizzbuzz(int i) {
+    public static int dofizzbuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
